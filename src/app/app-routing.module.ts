92e1058df837f41a91bf717e98596b26e7c44da7
stabri <paulstarbi@gmail.com>
2019-08-30 22:31:07 +0200

    basic navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {OfferComponent} from "./offer/offer.component";
+import {ContactComponent} from "./contact/contact.component";
+import {CompanyComponent} from "./company/company.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'oferta', component: OfferComponent},
+  {path: 'kontakt', component: ContactComponent},
+  {path: 'ofirmie', component: CompanyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FlexLayoutModule} from "@angular/flex-layout";
+import { HeaderComponent } from './header/header.component';
+import {MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule} from "@angular/material";
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { OfferComponent } from './offer/offer.component';
+import { ItemComponent } from './offer/item/item.component';
+import { CompanyComponent } from './company/company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ContactComponent,
+    OfferComponent,
+    ItemComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
